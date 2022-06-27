ae362f15723d328336bf8656cdebec5919a662d5
unknown <kelu33@gmail.com>
2022-06-28 01:17:25 +0200

    minify crack

--- src/app/shark/shark.component.ts
+++ src/app/shark/shark.component.ts
@@ -5,21 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './shark.component.html',
   styleUrls: ['./shark.component.css']
 })
-export class SharkComponent implements OnInit {
+export class SharkComponent {
 
-  public song: HTMLParagraphElement = document.createElement('p');
+  public lyrics: HTMLParagraphElement = document.createElement('p');
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  ngAfterViewInit(): void {
-    song();
-    console.log('------------------------------')    
-    son2();
+  ngAfterViewInit(): void {    
+    this.lyrics.innerHTML = this.s();
+    document.body.append(this.lyrics);
+    console.log('song function nb of characters : ' + this.charNb(this.S));
+    console.log('Shark Song :');
+    this.S();
   }
 
-  
+  charNb(fn: Function): number {
+    return fn.toString().length;
+  }
+
+  s(p=['Baby','Mommy','Daddy','Grandma','Grandpa',0],l=''){p.map(s=>{s?s+=' shark':s=`Let\'s go hunt`;for(let j=0;j<3;j++)l+=`${s}, ${'doo '.repeat(6)}<br>`;l+=`${s}!<br><br>`;});l+='Run away,…';return l}
+
+  S(p=['Baby','Mommy','Daddy','Grandma','Grandpa',0],l=''){p.map(s=>{s?s+=' shark':s=`Let\'s go hunt`;for(let j=0;j<3;j++)l+=`${s}, ${'doo '.repeat(6)}\n`;l+=`${s}!\n`;});l+='Run away,…';console.log(l)}
 }
 
 // OG => 455 chars
@@ -41,8 +45,4 @@ export class SharkComponent implements OnInit {
 // }
 
 // factorized => 251 chars
-const song=(p=['Baby','Mommy','Daddy','Grandma','Grandpa',`Let's go hunt`],l='',i=0)=>{let s=p[i];if(i<p.length-1)s+=' shark';for(let j=0;j<3;j++)l+=`${s}, ${'doo '.repeat(6)}\n`;l+=s+'!\n';++i<p.length?song(p,l,i):(l+='Run away,…',console.log(l));}
-
-
-// With map => 212 chars
-const son2=(p=['Baby','Mommy','Daddy','Grandma','Grandpa',0],l='')=>{p.map(s=>{s?s+=' shark':s=`Let\'s go hunt`;for(let j=0;j<3;j++)l+=`${s}, ${'doo '.repeat(6)}\n`;l+=`${s}!\n`;});l+='Run away,…';console.log(l)}
+// const song=(p=['Baby','Mommy','Daddy','Grandma','Grandpa',`Let's go hunt`],l='',i=0)=>{let s=p[i];if(i<p.length-1)s+=' shark';for(let j=0;j<3;j++)l+=`${s}, ${'doo '.repeat(6)}\n`;l+=s+'!\n';++i<p.length?song(p,l,i):(l+='Run away,…',console.log(l));}
