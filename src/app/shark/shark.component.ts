c8711041102b3e1db616fa28d7505e9b96fd1f3c
unknown <kelu33@gmail.com>
2022-06-29 21:54:19 +0200

    reduce shark

--- src/app/shark/shark.component.ts
+++ src/app/shark/shark.component.ts
@@ -12,24 +12,18 @@ export class SharkComponent {
   ngAfterViewInit(): void {    
     this.lyrics.innerHTML = this.song();
     document.body.append(this.lyrics);
-    console.log('S function nb of characters : ' + this.charNb(this.Song));
+    console.log('Song function nb of characters : ' + this.charNb(this.Song));
     console.log('Shark Song :');
-    this.Song();
+    console.log(this.Song());    
   }
 
   charNb(fn: Function): number {
     return fn.toString().length;
   }
 
-  song = (l='')=>{
-    ['Baby','Mommy','Daddy','Grandma','Grandpa',0].map(s=>{
-      s?s+=' shark':s=`Let\'s go hunt`
-      l+=`${s}, ${'doo '.repeat(6)}<br>`.repeat(3)+s+`!<br><br>`
-    })
-    return l+='Run away,…'
-  }
+  song(l=''){['Baby','Mommy','Daddy','Grandma','Grandpa',0].map(s=>{s?s+=' shark':s=`Let's go hunt`;l+=`${s}, ${'doo '.repeat(6)}<br>`.repeat(3)+s+`!<br><br>`});return l+='Run away,…'}
 
-  Song(l=''){['Baby','Mommy','Daddy','Grandma','Grandpa',0].map(s=>{s?s+=' shark':s=`Let\'s go hunt`;l+=`${s}, ${'doo '.repeat(6)}\n`.repeat(3);l+=`${s}!\n`;});console.log(l+='Run away,…')}
+  Song(l=''){['Baby','Mommy','Daddy','Grandma','Grandpa',0].map(s=>{s?s+=' shark':s=`Let's go hunt`;l+=`${s}, ${'doo '.repeat(6)}\n`.repeat(3);l+=`${s}!\n`;});return l+='Run away,…'}
 }
 
 // OG => 455 chars
