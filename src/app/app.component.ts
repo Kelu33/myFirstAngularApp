5aabadacdcd2f64168d75b2f353560054fb10527
Lucas Baste <kelu33@gmail.com>
2022-07-04 11:16:27 +0200

    clean branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { refValidator } from './custom-validators';
-import { Article } from './models/article.model';
 
 @Component({
   selector: 'app-root',
@@ -11,27 +8,5 @@ import { Article } from './models/article.model';
 export class AppComponent { 
   title = 'Lucas';
 
-  articlesList: Article[] = [];
-
-  articleForm: FormGroup = this.formBuilder.group({
-    ref: ['', [Validators.required, refValidator]],
-    name: ['', Validators.required],
-    description: ['', Validators.required]
-
-  })
-
-  constructor( private formBuilder: FormBuilder ) {}
-
-  onSubmit(): void {
-    if (this.articleForm.valid) {
-      const controls = this.articleForm.controls;
-      this.articlesList.push(
-        new Article(
-          controls['ref'].value,
-          controls['name'].value,
-          controls['description'].value
-        )
-      )
-    }
-  }
+  constructor( ) {}
 }
